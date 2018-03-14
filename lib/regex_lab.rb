785dcadc1b67c34e7aa04efc14a20c7a9b143068
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-14 19:20:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,6 +9,7 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
  if !text.scan(/\Aun+\w+ing\z/).empty?
    return text.scan(/\Aun+\w+ing\z/)
+ end
 end
 
 def words_five_letters_long(text)
