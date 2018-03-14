f2edca370386cbf73844b826289084b49b453063
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-14 19:21:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,9 +7,7 @@ end
 
 
 def words_starting_with_un_and_ending_with_ing(text)
- if !text.scan(/\Aun+\w+ing\z/).empty?
-   return text.scan(/\Aun+\w+ing\z/)
- end
+text.scan(/\Aun+\w+ing\z/)
 end
 
 def words_five_letters_long(text)
