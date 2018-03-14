0d09cae61e9e759b58cdea1304dccdebe26df6ea
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-14 19:00:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,8 +12,8 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
   array = []
 text.scan(/(\Aun)+\w+(ing\z)/)
-array = text.scan(/(\Aun)+\w+(ing\z)/)
-array
+array << text.scan(/(\Aun)+\w+(ing\z)/)
+return array
 end
 
 def words_five_letters_long(text)
