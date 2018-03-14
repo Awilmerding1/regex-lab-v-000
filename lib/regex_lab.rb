8d5acd84d60a629089becd705d9d39ad1e975d3a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-14 18:59:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
   array = []
 text.scan(/(\Aun)+\w+(ing\z)/)
-array << text.scan(/(\Aun)+\w+(ing\z)/)
+array = text.scan(/(\Aun)+\w+(ing\z)/)
 array
 end
 
